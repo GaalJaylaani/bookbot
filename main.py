f6e4c2627abfd34227788e7bbf46951f472a87b0
gaaljaylaani <GAALJAYLAANI123@GMAIL.COM>
2025-12-14 13:37:08 -0800

    working on ch3l2

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from stats import get_num_words, get_ch_num, chars_dictsorted
+import sys
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_dict = get_ch_num(text)
     chars_sorted_list = chars_dictsorted(char_dict)
-    
+    print("Usage: python3 main.py <path_to_book>")
     print_report(book_path, num_words, chars_sorted_list)
     
 def get_book_text(path):
