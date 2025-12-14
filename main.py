9264e5fc8ec9f0e15fea1f50177fb49e0ad5edc7
Gaal Jaylaani <gaal@example.com>
2025-12-14 13:50:20 -0800

    updated program so we could use any book path instead of hardcoded value

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import sys
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     book_path = sys.argv[1]
+
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_dict = get_ch_num(text)
     chars_sorted_list = chars_dictsorted(char_dict)
-    print("Usage: python3 main.py <path_to_book>")
     print_report(book_path, num_words, chars_sorted_list)
     
 def get_book_text(path):
